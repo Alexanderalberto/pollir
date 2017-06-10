942c1ff11f774ed645aa215090e2551d08d47466
Alexanderalberto <alex.3005.pdd.mh@gmail.com>
2017-06-10 14:39:32 -0500

    Add files via upload

---
+++ LA PÁGINA WEB/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	// Mobile nav functionality
+	$('#hamburger,#close').click(function() {
+		$('#hamburger,#close').toggle();
+		$('.custom-navigation #links').toggleClass('visible');
+	});
+
+});
